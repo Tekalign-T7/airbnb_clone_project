4872535d8aa00bcd1ec1bb6d473e94ab616f1831
Tekalign-T7 <tekaligntekola@gmail.com>
2025-10-24 21:09:05 +0300

    initial commit

---
+++ airbnb_project/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import "../App.css"
+import logo from "../images/airbnb 1.png"
+export function Navbar(){
+  return(
+    <nav>
+      <img src={logo} alt=" air bnb logo" className="logo"/>
+    </nav>
+  )
+
+}
